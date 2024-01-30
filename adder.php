b2baae48cb56c014110258cc920a59c10aca053c
Alexandra Ciancimino <noreply>
2024-01-29 23:29:06 -0800

    updates portal + adds adder to troubleshooting

---
+++ adder.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html> <!--Updated doctype-->
+<html> 
+<head>
+<title>My Adder Assignment</title>
+    <meta charset="UTF-8">
+<style>
+    p {
+        color:red;
+        text-align: center;
+    }
+    
+    h1 {
+        color:green;
+    }
+    
+    h2 {
+        font-size:1.5em;
+        text-align: center;
+    }
+    
+    form {
+        width:350px;
+        margin:0 auto;
+        border:1px solid red;
+        padding:10px;
+    }
+    
+    h1 {
+        text-align: center;
+    }
+    
+    
+</style>
+</head>
+<body>  
+    <h1>Adder.php</h1>   
+
+    <form action="" method="post">
+        <label> Enter the first number:</label>
+        <input type="text" name="num1"><br>  
+        <label>Enter the second number:</label>
+        <input type="text" name="num2"><br>  
+        <input type="submit" value="Add Em!!">   
+    </form> 
+
+    <?php     //adder-wrong.php
+
+    if (isset($_POST['num1'])){
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    // removed - and N
+    $myTotal = $num1 + $num2;
+    // removed spaces a changed " to ' 
+    echo '<h2>You added '.$num1.' and '.$num2.'</h2>';
+    // added span to style and removed extra " 
+    echo '<p> and the answer is <br><span style="color: red;">'.$myTotal.'!</span></p>';
+    // added space after echo and closed p tag
+    echo '<p><a href="">Reset page</a></p>';
+    }
+    ?>   
+         <!--Updated closing tags-->                     
+  </body>  
+  </html>
