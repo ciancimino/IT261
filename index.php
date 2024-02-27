a2c6ab7622be3391d01bda09e8cc5937b9d115e9
Alexandra Ciancimino <noreply>
2024-02-26 23:16:03 -0800

    adds pictures

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
         <ol>
             <li><a href="weeks/week7/form3.php">form3.php</a></li>
             <li><a href="weeks/week7/strings.php">strings.php</a></li>
-            <li><a href="">example3.php</a></li>
+            <li><a href="weeks/week7/pictures.php">pictures.php</a></li>
             <li><a href="">example4.php</a></li>
         </ol> 
 
