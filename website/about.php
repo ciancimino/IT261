79379b9b79ea313150db3fd3d3ea094acd13da0d
Alexandra Ciancimino <noreply>
2024-03-08 23:28:13 -0800

    updates about page on website

--- website/about.php
+++ website/about.php
@@ -3,8 +3,11 @@ include('config.php');
 include('./includes/header.php');
 ?>
 <main>
-<h1>Screenshot of Table </h1>
+<h1>Screenshot of Tables </h1>
 <p>
+<a href="fruits.php">
+    <img src="images/tabl2.jpg" alt="table screenshot">
+</a>
 <a href="fruits.php">
     <img src="images/table.jpg" alt="table screenshot">
 </a>
